ef71aff0b7895700e55c28e263a825e80fc6bb3d
Ricardo <ricardo.demauro@gmail.com>
2015-01-22 19:26:42 -0200

    Funcao transform

--- jquery-sortSelect.js
+++ jquery-sortSelect.js
@@ -3,40 +3,32 @@
 */
 (function ($) {
     $.fn.sortSelect = function (options) {
-        var opts = $.extend({
-            compareFunction: compare,
-            firstAttribute: 'text',
-            secondAttribute: 'value'
-        }, options);
+        var opts = $.extend({compareFunction: compare, firstAttribute: 'text',secondAttribute: 'value', transformFunction: transform}, options);
 
-        return this.each(function () {
-            __sortSelect(this);
-        });
+        return this.each(function () {__sortSelect(this);});
 
         function __sortSelect(selElem) {
-            var tmpAry = new Array();
+            var tmpAry = [];
             for (var i = 0; i < selElem.options.length; i++) {
-                tmpAry[i] = new Array();
-                tmpAry[i] = {
-                    first: selElem.options[i][opts.firstAttribute],
-                    second: selElem.options[i][opts.secondAttribute],
-                    elem: selElem.options[i]
+                tmpAry[i] = [];
+                tmpAry[i] = {first: selElem.options[i][opts.firstAttribute],second: selElem.options[i][opts.secondAttribute],elem: selElem.options[i]
                 };
             }
 
-
             tmpAry.sort(opts.compareFunction);
-            while (selElem.options.length > 0) {
-                selElem.options[0] = null;
-            }
+            while (selElem.options.length > 0) {selElem.options[0] = null;}
             for (var i = 0; i < tmpAry.length; i++) {
-                selElem.options[i] = tmpAry[i]['elem'];
+            	selElem.options[i] = opts.transformFunction(tmpAry[i]['elem']);
+            	selElem.options[i].selected = false;
             }
+            if(tmpAry.length > 0) {
+	            selElem.options[0].selected = true;
+	        }
             return;
         }
 
-        function compare(elem1, elem2) {
-            return elem1.first === elem2.first ? 0 : (elem1.first > elem2.first ? 1 : -1);
-        }
+        function compare(elem1, elem2) {return elem1.first === elem2.first ? 0 : (elem1.first > elem2.first ? 1 : -1);}
+        
+        function transform(elem1) { return elem1; }
     }
 }(jQuery));
